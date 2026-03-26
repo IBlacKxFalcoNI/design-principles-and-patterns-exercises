06f6f5f0818be42fa1ed8081d5677d1fd5410eb5
IBlacKxFalcoNI <alexander.kaiser136@yahoo.de>
2026-03-26 17:04:19 +0100

    ex 1 add money class

--- exercise_1.py
+++ exercise_1.py
@@ -5,10 +5,40 @@ from __future__ import annotations
 from decimal import Decimal
 
 
+
+class Money:
+    def __init__(self, amount: str | Decimal) -> None:
+        if isinstance(amount, str):
+            self._amount = Decimal(self._format_amount(amount))
+        else:
+            assert amount.same_quantum(self._get_representative_decimal())
+            self._amount = amount
+
+    def apply_discount(self, percentage: int) -> Money:
+        assert percentage >= 0 and percentage <= 100
+        discounted = self._amount * (1 - Decimal(percentage) / Decimal(100))
+        discounted = discounted.quantize(self._get_representative_decimal())
+        return Money(discounted)
+    
+    def _format_amount(self, money_str: str) -> str:
+        if "." not in money_str:
+            return f"{money_str}.00"
+        elif len(money_str.split(".")[1]) != 2:
+            raise IOError("Too many decimal digits")
+        return money_str
+    
+    def _get_representative_decimal(self) -> Decimal:
+        return Decimal("1.00")
+    
+    def __repr__(self) -> str:
+        return str(self._amount)
+    
+
 class Product:
-    def __init__(self, name: str, price: str) -> None:
+    def __init__(self, name: str, price: Money, has_discount: bool = False) -> None:
         self._name = name
-        self._price = self._price_from_string(price)
+        self._price = price
+        self._has_discount = has_discount
 
     def __repr__(self) -> str:
         return f"Product: {self._name}, price: {self._price}"
@@ -18,31 +48,17 @@ class Product:
         return self._name
 
     @property
-    def price(self) -> Decimal:
+    def price(self) -> Money:
         return self._price
 
     def reduced(self, percentage: int) -> Product:
         assert percentage >= 0 and percentage <= 100
-        assert not self._is_reduced()
-        reduced_fraction = self._price_from_string(str(percentage))/Decimal("100")
-        reduction = self._price*reduced_fraction
-        reduction = reduction.quantize(Decimal("1.00"))
+        assert not self._has_discount
         return Product(
             name=f"{self._name} (reduced)",
-            price=str(self._price - reduction)
+            price=self._price.apply_discount(percentage)
         )
 
-    def _price_from_string(self, price_str: str) -> Decimal:
-        if "." not in price_str:
-            return Decimal(f"{price_str}.00")
-        elif len(price_str.split(".")[1]) != 2:
-            raise IOError("Too many decimal digits")
-        return Decimal(price_str)
-
-    def _is_reduced(self) -> bool:
-        return "(reduced)" in self._name
-
-
 if __name__ == "__main__":
-    print(Product("Laptop", "999.00"))
-    print(Product("Laptop", "999.00").reduced(30))
+    print(Product("Laptop", Money("999.00")))
+    print(Product("Laptop", Money("999.00")).reduced(30))
